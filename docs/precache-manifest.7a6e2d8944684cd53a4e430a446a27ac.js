ed80bbb4f22e404d69906065a5172ba94479d71a
Patrick Jung <patrick.pddstudio@googlemail.com>
2018-05-02 10:50:06 +0200

    Added production build to test github-pages

---
+++ docs/precache-manifest.7a6e2d8944684cd53a4e430a446a27ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08277caf839b8a53b0e4",
+    "url": "/js/vendor.08277caf.js"
+  },
+  {
+    "revision": "bb41d6d80c1fdaad6874",
+    "url": "/js/manifest.bb41d6d8.js"
+  },
+  {
+    "revision": "4073cc787f458bfa0101",
+    "url": "/js/app.4073cc78.js"
+  },
+  {
+    "revision": "c670846c592502a030fd9b46f8fc3ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4073cc787f458bfa0101",
+    "url": "/css/app.cca9b56c.css"
+  }
+];
