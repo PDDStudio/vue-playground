e2f9d52b4c06f67284d5beab23d33ac31c7e99c6
Patrick Jung <patrick.pddstudio@googlemail.com>
2018-05-02 09:21:12 +0200

    Moved application layout into v-app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <div id="app">
+  <v-app>
+  <v-navigation-drawer app>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
-  </div>
+  </v-navigation-drawer>
+  <v-toolbar app></v-toolbar>
+  <v-content>
+    <v-container fluid>
+      <router-view></router-view>
+    </v-container>
+  </v-content>
+  <v-footer app></v-footer>]
+  </v-app>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
